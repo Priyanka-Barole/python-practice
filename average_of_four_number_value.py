0004f5b1a1051bce6ca5720ecddcd20a6d4c784d
Priyanka-Barole <priyankabarole888@gmail.com>
2023-12-21 18:55:52 +0530

    Update average_of_four_number_value.py

--- average_of_four_number_value.py
+++ average_of_four_number_value.py
@@ -4,3 +4,6 @@ var_average_of_number_value3 = int(input("enter average of number value m3:"))
 var_average_of_number_value4 = int(input("enter average of number value m4:"))	
 result_average_of_four_number_value = (var_average_of_number_value1+var_average_of_number_value2+var_average_of_number_value3+var_average_of_number_value4)/4
 print(result_average_of_four_number_value)
+
+
+# extra dummy line
