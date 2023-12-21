c90e729245b9b7e1ec3c73d805e93d72a513a7f3
Priyanak-Barole <priyankabarole888@gmail.com>
2023-12-21 17:07:12 +0530

    add average program

---
+++ average_of_four_number_value.py
@@ -0,0 +1,6 @@
+var_average_of_number_value1 = int(input("enter average of number value m1:"))
+var_average_of_number_value2 = int(input("enter average of number value m2:"))
+var_average_of_number_value3 = int(input("enter average of number value m3:"))
+var_average_of_number_value4 = int(input("enter average of number value m4:"))	
+result_average_of_four_number_value = (var_average_of_number_value1+var_average_of_number_value2+var_average_of_number_value3+var_average_of_number_value4)/4
+print(result_average_of_four_number_value)
